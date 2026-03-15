00c8511c0de6d7f1f3654834fbbcf3fe3cda3be9
pau-miralles <pmms0808@gmail.com>
2026-03-15 22:30:09 +0100

    Organizado y preparado para el SSG

---
+++ template/generator.py
@@ -0,0 +1,15 @@
+with open("songs.md", "r", encoding="utf-8") as f: lineas = f.readlines()
+
+# Abrir y leer archivo
+# Hacer un bucle infinito
+# Buscar # (nueva cancion)
+#
+# Entrar en un bucle de cancion.
+# Guardar Artista, Escala y BPM
+# Guardar Acordes (lista)
+# Guardar Letra (lista)
+# Linkear al audio.
+#
+# Añadir todo a la plantilla
+# Escribir la plantilla en un archivo nuevo
+# with open("songs.md", "w", encoding="utf-8") as f: f.writelines(lineas)
