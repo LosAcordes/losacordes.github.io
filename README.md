b96736d099c5c8b5af190676e06a41414b9307a9
pau-miralles <pmms0808@gmail.com>
2026-03-16 10:43:22 +0100

    escritura de plantilla. falta lectura del archivo

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Disponible en [losacordes.github.io](losacordes.github.io)
 > Nota: esta es la primera web que hago, asi que no es la mejor.
 ### Características:
 - Búsqueda de canciones por título o autor.
-- Modo claro y oscuro.
 - Información sobre las canciones:
     - Autor
     - BPM
@@ -11,3 +10,5 @@ Disponible en [losacordes.github.io](losacordes.github.io)
     - Acordes
     - Letra
     - Audio
+- Modo claro y oscuro, recordado por el navegador.
+- Páginas de las canciones generadas automáticamente por [un programa de python](template/generator.py) que interpreta [el listado de canciones](template/songs.md) (escrito en Markdown para su sencilla navegación y edición, *por lo que las letras de las canciones se verán mas compactas*).
--- template/generator.py
+++ template/generator.py
@@ -1,3 +1,4 @@
+with open("song-template.html", "r", encoding="utf-8") as f: plantilla = f.read()
 with open("songs.md", "r", encoding="utf-8") as f: lineas = f.readlines()
 
 # Abrir y leer archivo
@@ -5,11 +6,39 @@ with open("songs.md", "r", encoding="utf-8") as f: lineas = f.readlines()
 # Buscar # (nueva cancion)
 #
 # Entrar en un bucle de cancion.
+# Guardar Titulo
 # Guardar Artista, Escala y BPM
-# Guardar Acordes (lista)
-# Guardar Letra (lista)
-# Linkear al audio.
-#
-# Añadir todo a la plantilla
-# Escribir la plantilla en un archivo nuevo
-# with open("songs.md", "w", encoding="utf-8") as f: f.writelines(lineas)
+# Guardar Acordes
+# Guardar Letra
+
+lineatitulo = None
+for posicion, linea in enumerate(lineas):
+    if linea[0] == '#':
+        titulo = linea[2:-1]
+        lineatitulo = posicion
+        print(titulo, lineatitulo)
+
+titulo = 'Al Mar!'
+artista = 'Artista aqui'
+escala = 'Dm'
+bpm = '150 BPM'
+
+acordes = """Dm - C - G - A
+Dm - A - G - %"""
+
+letra ="""lalalala lala al
+la la la la la
+mas letra por aqui eyc eyc
+y acaba aqui"""
+
+plantilla = plantilla.format(
+    titulo=titulo,
+    artista=artista,
+    escala=escala,
+    bpm=bpm,
+    audio=titulo.replace(" ", "_"),
+    acordes=acordes,
+    letra=letra
+)
+
+# with open("web-generada.html", "w", encoding="utf-8") as f: f.write(plantilla)
