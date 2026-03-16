ebb88940e898876600f706e722c0b5b16e7bd987
pau-miralles <pmms0808@gmail.com>
2026-03-16 18:58:44 +0100

    Interpretador de python + añadir todas las canciones

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,9 +1,24 @@
 const links= document.getElementById("links");
 const searchBar = document.getElementById("searchBar");
 const canciones = [
-  { title: "Al mar! - Manel", link: "Al_mar!.html" },
-  { title: "Come Together - The Beatles", link: "Come_Together.html" },
+  { title: "Tobogàn - Zoo", link: "Tobogan.html" },
   { title: "Corbelles - Zoo", link: "Corbelles.html" },
+  { title: "Esbarzers (Remix La Gossa Sorda) - Zoo", link: "Esbarzers_(Remix_La_Gossa_Sorda).html" },
+  { title: "Al mar! - Manel", link: "Al_mar!.html" },
+  { title: "Killing in the Name - Rage Against the Machine", link: "Killing_in_the_Name.html" },
+  { title: "Zapatillas - El Canto del Loco", link: "Zapatillas.html" },
+  { title: "Mari Carmen - La Pegatina", link: "Mari_Carmen.html" },
+  { title: "Sweet Child O' Mine - Guns N' Roses", link: "Sweet_Child_O'_Mine.html" },
+  { title: "Sweet Home Alabama - Lynyrd Skynyrd", link: "Sweet_Home_Alabama.html" },
+  { title: "Come Together - The Beatles", link: "Come_Together.html" },
+  { title: "Despacito - Luis Fonsi", link: "Despacito.html" },
+  { title: "Tusa - KAROL G & Nicki Minaj", link: "Tusa.html" },
+  { title: "Seven Nation Army - The White Stripes", link: "Seven_Nation_Army.html" },
+  { title: "Coti X Coti - The Tyets", link: "Coti_X_Coti.html" },
+  { title: "Cul - Oques Grasses", link: "Cul.html" },
+  { title: "Fent voltes per Palma - Xanguito & Tomeu Penya", link: "Fent_voltes_per_Palma.html" },
+  { title: "LA PENÚLTIMA - La Fúmiga", link: "LA_PENULTIMA.html" },
+  { title: "Smells Like Teen Spirit - Nirvana", link: "Smells_Like_Teen_Spirit.html" },
 ];
 
 searchBar.addEventListener("keyup", (e) => {
--- template/generator.py
+++ template/generator.py
@@ -1,44 +1,35 @@
+import unicodedata
+def remove_accents(string): return "".join(c for c in unicodedata.normalize("NFD", string) if not unicodedata.combining(c)) 
+
+def crear_archivo(titulo, artista, escala, bpm, inicio_acordes, fin_acordes, posicion):
+    acordes = ''.join(lineas[inicio_acordes+1:fin_acordes])[:-1]
+    letra = ''.join(lineas[fin_acordes+2:posicion-1])[:-1]
+    nombre_archivo = remove_accents(titulo).replace(' ', "_")
+
+    rellenado = plantilla.format(
+        titulo=titulo,
+        artista=artista,
+        escala=escala,
+        bpm=bpm,
+        audio=nombre_archivo,
+        acordes=acordes,
+        letra=letra)
+    with open("../songs/"+nombre_archivo+".html", "w", encoding="utf-8") as f: f.write(rellenado)
+    print("{"+f" title: \"{titulo} - {artista}\", link: \"{nombre_archivo}.html\" "+"},")
+
 with open("song-template.html", "r", encoding="utf-8") as f: plantilla = f.read()
 with open("songs.md", "r", encoding="utf-8") as f: lineas = f.readlines()
 
-# Abrir y leer archivo
-# Hacer un bucle infinito
-# Buscar # (nueva cancion)
-#
-# Entrar en un bucle de cancion.
-# Guardar Titulo
-# Guardar Artista, Escala y BPM
-# Guardar Acordes
-# Guardar Letra
-
-lineatitulo = None
+titulo, inicio_acordes, fin_acordes = None, None, None
 for posicion, linea in enumerate(lineas):
     if linea[0] == '#':
+        if titulo != None: crear_archivo(titulo, artista, escala, bpm, inicio_acordes, fin_acordes, posicion)
         titulo = linea[2:-1]
-        lineatitulo = posicion
-        print(titulo, lineatitulo)
+        artista = lineas[posicion+1][2:-1]
+        escala = lineas[posicion+2][2:-1]
+        bpm = lineas[posicion+3][2:-1]
+    if linea[0:3] == '```':
+        inicio_acordes = fin_acordes
+        fin_acordes = posicion
+else: crear_archivo(titulo, artista, escala, bpm, inicio_acordes, fin_acordes, posicion)
 
-titulo = 'Al Mar!'
-artista = 'Artista aqui'
-escala = 'Dm'
-bpm = '150 BPM'
-
-acordes = """Dm - C - G - A
-Dm - A - G - %"""
-
-letra ="""lalalala lala al
-la la la la la
-mas letra por aqui eyc eyc
-y acaba aqui"""
-
-plantilla = plantilla.format(
-    titulo=titulo,
-    artista=artista,
-    escala=escala,
-    bpm=bpm,
-    audio=titulo.replace(" ", "_"),
-    acordes=acordes,
-    letra=letra
-)
-
-# with open("web-generada.html", "w", encoding="utf-8") as f: f.write(plantilla)
