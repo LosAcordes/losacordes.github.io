74bf474f0269ee86eca28b64d6bf1f359a86a76c
pau-miralles <pmms0808@gmail.com>
2026-03-28 00:04:33 +0100

    arreglar almacenamiento service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'losacordes-v1.2.3';
+const CACHE_NAME = 'losacordes-v1.2.4';
 
 const urlsToCache =[
   "./",
@@ -7,6 +7,7 @@ const urlsToCache =[
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
+
   "./js/actualizacion.js",
   "./js/canciones.js",
   "./js/darkmode.js",
@@ -21,6 +22,7 @@ const urlsToCache =[
   "./audio/Coti_X_Coti.mp3",
   "./audio/Cul.mp3",
   "./audio/Despacito.mp3",
+  "./audio/Die_With_A_Smile.mp3",
   "./audio/Don't_Start_Now.mp3",
   "./audio/Esbarzers_(Remix_La_Gossa_Sorda).mp3",
   "./audio/Fent_voltes_per_Palma.mp3",
@@ -29,13 +31,18 @@ const urlsToCache =[
   "./audio/Killing_in_the_Name.mp3",
   "./audio/LA_PENULTIMA.mp3",
   "./audio/Mari_Carmen.mp3",
+  "./audio/Mr_PC.mp3",
+  "./audio/Nakamarra.mp3",
   "./audio/September.mp3",
   "./audio/Seven_Nation_Army.mp3",
   "./audio/Smells_Like_Teen_Spirit.mp3",
   "./audio/Sweet_Child_O'_Mine.mp3",
   "./audio/Sweet_Home_Alabama.mp3",
+  "./audio/The_Bright_Side.mp3",
+  "./audio/There_Will_Never_Be_Another_You.mp3",
   "./audio/Tobogan.mp3",
   "./audio/Tusa.mp3",
+  "./audio/Uptown_Funk.mp3",
   "./audio/WILDFLOWER.mp3",
   "./audio/Zapatillas.mp3",
 
@@ -54,6 +61,7 @@ const urlsToCache =[
   "./songs/Coti_X_Coti.html",
   "./songs/Cul.html",
   "./songs/Despacito.html",
+  "./songs/Die_With_A_Smile.html",
   "./songs/Don't_Start_Now.html",
   "./songs/Esbarzers_(Remix_La_Gossa_Sorda).html",
   "./songs/Fent_voltes_per_Palma.html",
@@ -62,13 +70,18 @@ const urlsToCache =[
   "./songs/Killing_in_the_Name.html",
   "./songs/LA_PENULTIMA.html",
   "./songs/Mari_Carmen.html",
+  "./songs/Mr_PC.html",
+  "./songs/Nakamarra.html",
   "./songs/September.html",
   "./songs/Seven_Nation_Army.html",
   "./songs/Smells_Like_Teen_Spirit.html",
   "./songs/Sweet_Child_O'_Mine.html",
   "./songs/Sweet_Home_Alabama.html",
+  "./songs/The_Bright_Side.html",
+  "./songs/There_Will_Never_Be_Another_You.html",
   "./songs/Tobogan.html",
   "./songs/Tusa.html",
+  "./songs/Uptown_Funk.html",
   "./songs/WILDFLOWER.html",
   "./songs/Zapatillas.html"
 ];
@@ -76,8 +89,15 @@ const urlsToCache =[
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting()) // Force the new service worker to activate immediately
+      .then(async (cache) => {
+        for (let url of urlsToCache) {
+          try {
+            await cache.add(url);
+          } catch (e) {
+          }
+        }
+      })
+      .then(() => self.skipWaiting()) 
   );
 });
 
@@ -91,25 +111,29 @@ self.addEventListener('activate', event => {
           }
         })
       );
-    }).then(() => self.clients.claim()) // Forces all open pages to use this new service worker
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
+    caches.match(event.request, { ignoreSearch: true }).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
 
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        return response;
-      })
-      .catch(() => {
-        // Fall back to cache if online
-        return caches.match(event.request);
-      })
+      return fetch(event.request)
+        .then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          }
+          return networkResponse;
+        })
+        .catch(error => {
+        });
+    })
   );
 });
